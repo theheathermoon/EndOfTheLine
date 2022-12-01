d72a7e085dbb1bbd5deadf3bd5108fff08cf072b
tt-digit <ttdigit.tv@gmail.com>
2022-11-30 20:27:47 -0800

    Fixed the pause menu and added some buttons

--- End_of_the_Line/Assets/Scripts/GameManager.cs
+++ End_of_the_Line/Assets/Scripts/GameManager.cs
@@ -22,12 +22,22 @@ public class GameManager : MonoBehaviour
 
     public static bool isPaused = false;
     public GameObject pauseUI;
+    public GameObject settingsUI;
+    public GameObject audioUI;
+    public GameObject brightnessUI;
+
+    public GameObject curUI;
+    public GameObject placeholderUI;
+    public GameObject lastUI;
 
     private void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
         pauseUI.SetActive(false);
+        settingsUI.SetActive(false);
+        audioUI.SetActive(false);
+        brightnessUI.SetActive(false);
     }
 
     private void Update()
@@ -54,6 +64,7 @@ public class GameManager : MonoBehaviour
     public void PauseGame()
     {
         isPaused = true;
+        curUI = pauseUI;
         pauseUI.SetActive(true);
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.Confined;
@@ -66,6 +77,39 @@ public class GameManager : MonoBehaviour
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Confined;
     }
+
+    public void ReturnToPause()
+    {
+        curUI.SetActive(false);
+        lastUI = curUI;
+        curUI = pauseUI;
+        curUI.SetActive(true);
+    }
+
+    public void OpenSettings()
+    {
+        curUI.SetActive(false);
+        lastUI = curUI;
+        curUI = settingsUI;
+        curUI.SetActive(true);
+    }
+
+    public void OpenAudio()
+    {
+        curUI.SetActive(false);
+        lastUI = curUI;
+        curUI = audioUI;
+        curUI.SetActive(true);
+    }
+
+    public void OpenBrightness()
+    {
+        curUI.SetActive(false);
+        lastUI = curUI;
+        curUI = brightnessUI;
+        curUI.SetActive(true);
+    }
+
     #endregion
     #region beacons
     public void SetActiveBeacon(GameObject newBeacon)
