ba1d440e54a629b8026d92b96be88c98fd0bcb27
theheathermoon <heatherkbyrom@gmail.com>
2022-12-15 13:00:54 +0000

    adding particle effect

---
+++ End_of_the_Line/Assets/Floating Dust & Falling Ash/Demo Scenes/Script/UIScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour {
+
+    public GameObject[] prefabsAsh;
+    public GameObject[] prefabsDust;
+    public Text[] buttonText;
+    public GameObject objWindzone;
+    Color defaultColor = new Color(50f / 255f, 50f / 255f, 50f / 255f);
+
+    public void ShowEffect1()
+    {
+        HideVFX();
+        ResetTextColor();
+
+        if (prefabsAsh[0]!=null) prefabsAsh[0].SetActive(true);
+        if (prefabsDust[0] != null) prefabsDust[0].SetActive(true);
+
+        buttonText[0].color = Color.red;
+    }
+
+
+    public void ShowEffect2()
+    {
+        HideVFX();
+        ResetTextColor();
+
+        if (prefabsAsh[1] != null) prefabsAsh[1].SetActive(true);
+        if (prefabsDust[1] != null) prefabsDust[1].SetActive(true);
+
+        buttonText[1].color = Color.red;
+    }
+
+    public void ShowEffect3()
+    {
+        HideVFX();
+        ResetTextColor();
+
+        if (prefabsAsh[2] != null) prefabsAsh[2].SetActive(true);
+        if (prefabsDust[2] != null) prefabsDust[2].SetActive(true);
+
+        buttonText[2].color = Color.red;
+    }
+
+    void HideVFX()
+    {
+        for (int i = 0; i < prefabsAsh.Length; i++)
+        {
+            if (prefabsAsh[i] != null) prefabsAsh[i].SetActive(false);
+        }
+
+        for (int i = 0; i < prefabsDust.Length; i++)
+        {
+            if (prefabsDust[i] != null) prefabsDust[i].SetActive(false);
+        }
+
+    }
+
+    void ResetTextColor()
+    {
+        for (int i = 0; i < buttonText.Length; i++)
+        {
+            buttonText[i].color = defaultColor;
+        }
+    }
+
+    public void SwitchWindzone(bool Selected) {
+        objWindzone.SetActive(Selected);
+    }
+
+}
---
+++ End_of_the_Line/Assets/Floating Dust & Falling Ash/ReadMe.txt
@@ -0,0 +1,23 @@
+---------------------------
+Floating Dust & Falling Ash
+---------------------------
+
+This package contains 6 Falling Ash effects + 3 Floating Dust effects ready to use in your projects. Just drag & drop a prefab into your scene then you will see the effect. 
+
+Contains:
+16 ash/dust shapes
+
+6 Falling Ash effects:
+Falling Ash - Light
+Falling Ash - Medium
+Falling Ash - Heavy
+Falling Ash (dark) - Light
+Falling Ash (dark) - Medium
+Falling Ash (dark) - Heavy
+
+3 Floating Dust effects:
+Floating Dust - Light
+Floating Dust - Medium
+Floating Dust - Heavy
+
+4 demo scenes
